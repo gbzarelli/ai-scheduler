c7cea203cd7b4979fee942dc8c0562adf801edf1
root <root@gbzarelli-nitro5.>
2025-08-29 09:36:38 -0300

    fix: pom configs

--- gcalendar-mcp-server/src/main/java/com/example/demo/DemoApplication.java
+++ gcalendar-mcp-server/src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,10 @@
 package com.example.demo;
 
-import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.Events;
 import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
-
-import java.io.IOException;
-import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
