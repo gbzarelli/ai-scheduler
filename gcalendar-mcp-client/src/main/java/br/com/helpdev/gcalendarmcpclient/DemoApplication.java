62a328e981f91c0d5d140b362795da408f3b3c98
root <root@gbzarelli-nitro5.>
2025-08-29 10:02:21 -0300

    feat: controller

--- gcalendar-mcp-client/src/main/java/br/com/helpdev/gcalendarmcpclient/DemoApplication.java
+++ gcalendar-mcp-client/src/main/java/br/com/helpdev/gcalendarmcpclient/DemoApplication.java
@@ -17,33 +17,4 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Bean
-    public int runAfterStartup(ChatClient chatClient) {
-        final var toClient = "16988056630";
-        final var fromRequester = "16988030791";
-        final var chatId = toClient + "-" + fromRequester;
-        final var userPromptTemplate = new PromptTemplate("""
-                    "toClient": "{toClient}",
-                    "fromRequester": "{fromRequester}",
-                    "dataHora": "{dataHora}",
-                    "message": "{message}",
-                """);
-
-        final var userPrompt = userPromptTemplate.create(
-                new HashMap<>() {{
-                    put("toClient", toClient);
-                    put("fromRequester", fromRequester);
-                    put("dataHora", LocalDateTime.now());
-                    put("message", "Qual meu próximo compromisso?");
-                }}
-        );
-        ChatClient.CallResponseSpec res = chatClient
-                .prompt()
-                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, chatId))
-                .user(userPrompt.getContents())
-                .call();
-        System.out.println(res.content());
-        return 1;
-    }
-
 }
--- gcalendar-mcp-client/src/main/java/br/com/helpdev/gcalendarmcpclient/controller/ChatController.java
+++ gcalendar-mcp-client/src/main/java/br/com/helpdev/gcalendarmcpclient/controller/ChatController.java
@@ -2,18 +2,53 @@ package br.com.helpdev.gcalendarmcpclient.controller;
 
 import br.com.helpdev.gcalendarmcpclient.controller.dto.RequestChat;
 import br.com.helpdev.gcalendarmcpclient.controller.dto.ResponseChat;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
 @RestController
 public class ChatController {
 
+    private final ChatClient chatClient;
 
-     @PostMapping("/chat")
-     public ResponseChat chat(@RequestBody RequestChat message) {
-return null;
-     }
+    private static final String PROMPT_TEMPLATE = """
+            "toClient": "{toClient}",
+            "fromRequester": "{fromRequester}",
+            "dataHora": "{dataHora}",
+            "message": "{message}",
+        """;
+
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @PostMapping("/chat")
+    public ResponseChat chat(@RequestBody RequestChat message) {
+        final var chatId = message.toClient() + "-" + message.fromRequester();
+        final var userPromptTemplate = new PromptTemplate(PROMPT_TEMPLATE);
+
+        final var userPrompt = userPromptTemplate.create(
+                new HashMap<>() {{
+                    put("toClient", message.toClient());
+                    put("fromRequester", message.fromRequester());
+                    put("dataHora", LocalDateTime.now());
+                    put("message", message.message());
+                }}
+        );
+        ChatClient.CallResponseSpec res = chatClient
+                .prompt()
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, chatId))
+                .user(userPrompt.getContents())
+                .call();
+
+        return new ResponseChat(message.toClient(), message.fromRequester(), res.content());
+    }
 }
 
 
